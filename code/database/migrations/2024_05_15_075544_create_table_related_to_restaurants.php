82cc46d6038d144fa13849dc820ae915d13f89c9
Amy Liu <juliet6124amy@gmail.com>
2024-05-21 17:35:11 +0800

    writing migration 075544

--- code/database/migrations/2024_05_15_075544_create_table_related_to_restaurants.php
+++ code/database/migrations/2024_05_15_075544_create_table_related_to_restaurants.php
@@ -11,8 +11,26 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('table_related_to_restaurants', function (Blueprint $table) {
+        Schema::create('favorites', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger("restaurant_id");
+            $table->foreign('restaurant_id')->references('id')->on('restaurants');
+            $table->bigInteger("user_id");
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->dateTime('opening_time', $precision = 0);
+            $table->dateTime('closing_time', $precision = 0);
+            $table->string('rest_day');
+            $table->tinyInteger('status');
+            $table->float('avg_score', 2, 1);
+            $table->integer('total_comments_count');
+            $table->integer('priority');
             $table->timestamps();
         });
     }
@@ -22,6 +40,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_related_to_restaurants');
+        Schema::dropIfExists('favorites');
+        Schema::dropIfExists('restaurants');
     }
 };
