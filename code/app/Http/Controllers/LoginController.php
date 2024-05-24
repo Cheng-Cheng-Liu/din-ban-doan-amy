3a6a6138b2d2ab83a95d08b0ff067351061163c5
Amy Liu <juliet6124amy@gmail.com>
2024-05-24 17:52:30 +0800

    personal_access_token

--- code/app/Http/Controllers/LoginController.php
+++ code/app/Http/Controllers/LoginController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
 use App\Models\Admin;
+use App\Models\Personal_access_token;
 
 class LoginController extends Controller
 {
@@ -21,25 +22,36 @@ class LoginController extends Controller
         $this->email = $request->input('email');
         $this->password = $request->input('password');
     }
-// 前台會員登入
-    public function memberLogin(Request $request){
+    // 前台會員登入
+    public function memberLogin(Request $request)
+    {
         // 檢查參數正確嗎?
         $checkParameter = $this->checkParameter();
         if ($checkParameter->fails()) {
-            return response()->json(['error'=>1001]);
+            return response()->json(['error' => 1001]);
         }
 
 
 
         $credentials = $request->only('email', 'password');
         if ($token = auth()->attempt($credentials)) {
+            $id = Auth::user()->id;
+            $time = now();
+            $personal_access_token = new Personal_access_token();
+            $personal_access_token->tokenable_type = "App\Models\User";
+            $personal_access_token->tokenable_id = $id;
+            $personal_access_token->name = "front";
+            $personal_access_token->token = $token;
+            $personal_access_token->created_at = $time;
+            $personal_access_token->save;
             return $this->respondWithToken($token);
         } else {
             return response()->json(['message' => 'Invalid email or password'], 401);
         }
     }
-// 後台會員登入
-    public function adminLogin(Request $request){
+    // 後台會員登入
+    public function adminLogin(Request $request)
+    {
 
         $credentials = $request->only('email', 'password');
         if ($token = auth()->guard('admin')->attempt($credentials)) {
@@ -48,7 +60,7 @@ class LoginController extends Controller
             return response()->json(['message' => 'Invalid email or password'], 401);
         }
     }
-// 驗證器
+    // 驗證器
     public function checkParameter()
     {
         $validator = Validator::make([
@@ -59,14 +71,13 @@ class LoginController extends Controller
             'password' => ['required', Password::min(6)],
         ]);
         return $validator;
-
     }
 
     protected function respondWithToken($token)
     {
         return response()->json([
-            "message"=> "登入成功",
-            "token"=>$token,
+            "message" => "登入成功",
+            "token" => $token,
         ]);
     }
 }
